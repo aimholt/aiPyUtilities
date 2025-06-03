ce04a2a2bbfff81ac62eff812d58ec5e7bb9d2ab
Andreas Imholt <andreas@imholt.de>
2025-06-04 01:15:47 +0200

    adding arg processing to imgage_geo_update

--- aipy/img_extensions.py
+++ aipy/img_extensions.py
@@ -5,9 +5,10 @@
 import os
 import gpxpy
 import piexif
+import re
 from zoneinfo import ZoneInfo
 from datetime import datetime
-import re
+from fractions import Fraction
 
 TZONE='Europe/Berlin'
 
@@ -27,11 +28,11 @@ def get_geo_from_gpx(fname):
                     })
     return gpx_by_date
 
-def print_exif_data(fname, dir):
+def print_exif_data(path):
     """
         print all exif data of a image file
     """    
-    exif_dict = piexif.load(os.path.join(dir,fname))
+    exif_dict = piexif.load(path)
     for ifd_name in exif_dict:
         if ifd_name != 'thumbnail':
             print(f'IFD-name: <{ifd_name}> IFD-elements: <{len(exif_dict[ifd_name])}>')
@@ -70,3 +71,76 @@ def get_file_list(dir, m_pattern=''):
         if os.path.isfile(path) and p.match(fname):
             path_list.append(path)
     return path_list
+
+def deg_to_dms(coord_dec, type):
+    """ 
+    this function converts decimal coordinates into the DMS (degrees, minutes and seconds) format.
+    It also determines the cardinal direction of the coordinates.
+    """
+    if      coord_dec > 0 and type=='lat':
+        direction = 'N'
+    elif    coord_dec < 0 and type=='lat':
+        direction = 'S'
+    elif    coord_dec > 0 and type=='lon':
+        direction = 'E'
+    elif    coord_dec < 0 and type=='lon':
+        direction = 'W'
+    else:
+        direction = "-"
+
+    deg = int(abs(coord_dec))
+    decimal_minutes = (abs(coord_dec) - deg) * 60
+    min = int(decimal_minutes)
+    sec = Fraction((decimal_minutes - min) * 60).limit_denominator(100)
+
+    #deg, coord_dec = divmod(coord_dec * 60, 60)
+    #min, sec = divmod(coord_dec * 60, 60)
+    ##return int(deg), int(min), float(sec), direction
+    return deg, min, sec, direction
+
+def dms_to_exif(dms_degrees, dms_minutes, dms_seconds):
+    """
+    This function converts DMS (degrees, minutes and seconds) to values that can
+    be used with the EXIF (Exchangeable Image File Format).
+
+    :param dms_degrees: int value for degrees
+    :param dms_minutes: int value for minutes
+    :param dms_seconds: fractions.Fraction value for seconds
+    :return: EXIF values for the provided DMS values
+    :rtype: nested tuple
+    """
+    exif_format = (
+        (dms_degrees, 1),
+        (dms_minutes, 1),
+        (int(dms_seconds.limit_denominator(100).numerator), int(dms_seconds.limit_denominator(100).denominator))
+    )
+    return exif_format
+
+def set_geo2exif(coord, img_path):
+    """
+    This function adds GPS values to an image using the EXIF format.
+    This fumction calls the functions deg_to_dms and dms_to_exif_format.
+
+    :param image_path: image to add the GPS data to
+    :param latitude: the north–south position coordinate
+    :param longitude: the east–west position coordinate
+    """    
+    ### convert coords from dec to dms
+    lat_dms=deg_to_dms(coord['lat'],type='lat')
+    lon_dms=deg_to_dms(coord['lon'],type='lon')
+    ### convert dms from to exif format
+    lat_exif = dms_to_exif(lat_dms[0], lat_dms[1], lat_dms[2])
+    lon_exif = dms_to_exif(lon_dms[0], lon_dms[1], lon_dms[2])
+
+    gps_coords = {
+        piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+        piexif.GPSIFD.GPSLatitude: lat_exif,
+        piexif.GPSIFD.GPSLatitudeRef: lat_dms[3],
+        piexif.GPSIFD.GPSLongitude: lon_exif,
+        piexif.GPSIFD.GPSLongitudeRef: lon_dms[3]
+        }
+    exif_dict = piexif.load(img_path)
+    exif_dict['GPS'] = gps_coords
+    exif_bytes = piexif.dump(exif_dict)
+    piexif.insert(exif_bytes, img_path)
+    return
--- image_geo_update.py
+++ image_geo_update.py
@@ -1,19 +1,48 @@
 """
     program to update the geo coordinates in an .jpg image file
 """
-from aipy.img_extensions import get_file_list, get_geo_from_gpx, get_img_timestamp, print_exif_data
-
 import sys
+import os
+from argparse import ArgumentParser
+from aipy.img_extensions import get_file_list, get_geo_from_gpx, get_img_timestamp, \
+                                set_geo2exif, print_exif_data
 
 if sys.platform.startswith('linux'):
     DIR='/home/aimholt/projects/TestData/pictures_tracklogs'
 elif sys.platform.startswith('win'):
     DIR='C:\\Users\\Andreas\\projects\\TestData\\pictures_tracklogs'
 
+parser=ArgumentParser(
+    prog='image GEO EXIF change',
+    description='backend-tool to update image geo data from tracking tools')
+group1=parser.add_mutually_exclusive_group(required=True)
+group1.add_argument('-i', '--image',
+            help='select only image with filename',
+            type=str)
+group1.add_argument('-a', '--all',
+            help='select all images in working directory',
+            action='store_true')
+parser.add_argument('-e', '--exif',
+            help='print current exif data',
+            action='store_true', default=False)
+parser.add_argument('-s', '--save',
+            help='save the changes to images',
+            action='store_true', default=False)
+parser.add_argument('-d', '--directory',
+            help='select working directory, current is: '+DIR,
+            type=str)
+args=parser.parse_args()
+
 TZONE='Europe/Berlin'
 
-### making list of files in working directory
-img_file_list=get_file_list(dir=DIR,m_pattern='^.*\\.(jpg)$')
+### making image filelist
+img_file_list=[]
+if args.all:
+    img_file_list=get_file_list(dir=DIR,m_pattern='^.*\\.(jpg)$')
+if args.image:
+    img_file_list.append(os.path.join(DIR,args.image))
+
+### making gpx file list
 gpx_file_list=get_file_list(dir=DIR,m_pattern='^.*\\.(gpx)$')
 
 ### making list of geo coordinates from multiple gpx files sorted by time 
@@ -22,16 +51,21 @@ for file in gpx_file_list:
     for item in get_geo_from_gpx(file):
         coord_list.append(item)        
 coord_list.sort(key=lambda x: x['dt'], reverse=False)
-print(f'number of geo entries: {len(coord_list)} - oldest: {coord_list[0]['dt']} - newest: {coord_list[len(coord_list)-1]['dt']}')
+print(f'number of geo entries found: {len(coord_list)} - oldest: {coord_list[0]['dt']} - newest: {coord_list[len(coord_list)-1]['dt']}')
+
 ### for each file open and read the timestamp
 count=0
 for file in img_file_list:
+    if args.exif:
+        print("\n>>>>>>>>>>>>>>>>>>>>>>>>> EXIF data of image: "+file)
+        print_exif_data(os.path.join(DIR,file))
+
     found=False
     coord_dt_before=None
     coord_dt_after=None
     img_dt=     get_img_timestamp(file)
     img_name=   file.lstrip(DIR)
-    max_time_diff=30
+    max_time_diff=60
 
     ### for each image find the best time matches in geo coordinates
     time_diff_before=max_time_diff
@@ -53,6 +87,9 @@ for file in img_file_list:
             f'{img_name:<20s} IMG({img_dt.date()} {img_dt.time()}) - ' 
             f'COORD({coord['dt'].date()} {coord['dt'].time()}; LAT: {coord['lat']:9.6f}; LON: {coord['lon']:9.6f};  0s)'
             )
+        ### set coords to image file
+        if args.save:
+            set_geo2exif(coord, file)
         found=False
     elif not found:
         if  coord_dt_before != None:
@@ -61,14 +98,22 @@ for file in img_file_list:
             time_diff_after = (coord_dt_after - img_dt).seconds
         if  (time_diff_before < max_time_diff or time_diff_after  < max_time_diff)  and \
             time_diff_before <= time_diff_after:
+            if args.save:
+                set_geo2exif(coord, file)
             print(
                 f'{img_name:<20s} IMG({img_dt.date()} {img_dt.time()}) - '
                 f'COORD({coord_dt_before.date()} {coord_dt_before.time()}; LAT: {coord['lat']:9.6f}; LON: {coord['lon']:9.6f}; -{time_diff_before}s)'
                 )
         elif(time_diff_before < max_time_diff or time_diff_after  < max_time_diff)  and \
             time_diff_before > time_diff_after:
+            if args.save:
+                set_geo2exif(coord, file)
             print(
                 f'{img_name:<20s} IMG({img_dt.date()} {img_dt.time()}) - '
                 f'COORD({coord_dt_after.date()} {coord_dt_after.time()}; LAT: {coord['lat']:9.6f}; LON: {coord['lon']:9.6f}; +{time_diff_after}s)'
             )
+if args.save:
+    print('>> changes saved to images')
+else:
+    print('>> nothing saved to images(only with option "-s")')
 exit()
