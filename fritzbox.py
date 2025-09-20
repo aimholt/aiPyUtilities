c983c08f04ba7fd876e8959d60916f414d5dbbb8
Andreas Imholt <andreas@imholt.de>
2025-09-20 10:22:55 +0200

    changes to http handling to fritzbox

--- fritzbox.py
+++ fritzbox.py
@@ -3,14 +3,17 @@ import urllib3
 import xml.etree.ElementTree as ET
 import hashlib
 import subprocess
+import requests
 import json
 import pandas as pd
 import os
 
-ROUTER_URL = "http://xyzrouter.internal"
-USER = "aimholt"  # Anmeldename z. B. fritz1234
-PASSWORD = "Imh2And-01"  # Anmeldekennwort
+ROUTER_URL =    "http://xyzrouter.internal"
+USER =          "aimholt"  # Anmeldename z. B. fritz1234
+PASSWORD =      "Imh2And-01"  # Anmeldekennwort
 NOT_VALID_SID = "0000000000000000" # immer ungueltig
+#DATA_DIR =      "/home/aimholt/projects/TestData/"
+DATA_DIR =      "C:\\Users\\Andreas\\projects\\TestData"
 
 def fb_get_sid(fritzbox, fritz_user, fritz_pw):
     fb_sid=NOT_VALID_SID
@@ -62,15 +65,40 @@ def get_struct(x, level=0, struct=[]):
             level-=1
     return struct
 
-def get_log(fb_sid=None, file=None):
-    if file and fb_sid:
-        with open('/home/aimholt/projects/TestData/' + file, 'w') as f:
-            f.write(xs)
-        log="data written to" + '/home/aimholt/projects/TestData/' + file
-    else:
+def get_log_from_fb(fb_sid=None, file=None):
+    url=ROUTER_URL+'/data.lua'
+    path=DATA_DIR+os.sep+file
+    payload={
+        'sid':          fb_sid,
+        'lang':         'de',
+        'page':         'log',
+        'no_siderenew': '',
+        }
+    resp=requests.post(url,data=payload)
+    if resp.status_code == 200 or resp.status_code == 200:
+        log=resp.text
+        with open(path, 'w') as f:
+            f.write(log)
+    return
+
+def get_log_from_fbXXX(fb_sid=None, file=None):
+    """
+        get log from fritzbox and write it to 'file' or return the log
+    """
+    if fb_sid and file:
+        path=DATA_DIR+os.sep+file
         cmd='wget -q -O - --post-data="sid=' + fb_sid + '&lang=de&page=log&no_sidrenew=" http://xyzrouter.internal/data.lua'
         xs=subprocess.check_output(cmd, shell=True, executable="/bin/bash",text=True) 
-        log=json.loads(xs)
+        with open(path, 'w') as f:
+            f.write(cmd)
+        msg="data written to" + path
+    elif fb_sid and not file:
+        cmd='wget -q -O - --post-data="sid=' + fb_sid + '&lang=de&page=log&no_sidrenew=" http://xyzrouter.internal/data.lua'
+        xs=subprocess.check_output(cmd, shell=True, executable="/bin/bash",text=True) 
+        msg=json.loads(xs)
+    else:
+        msg="error in arguments"
+
     #for i in get_struct(x):
     #    print(i)
     #log=[]
@@ -80,27 +108,27 @@ def get_log(fb_sid=None, file=None):
     #            if key2 == 'log':
     #                for x in x[key1][key2]:
     #                    log.append(f'{x['date']} {x['time']} {x['group']:>4} {x['msg']}')
-    return(log)
+    return(msg)
 
 def get_data_from_file(file):
     """ 
         reading json data from file 
     """
-    with open('/home/aimholt/projects/TestData/'+ file, 'r') as f:
+    path=DATA_DIR+os.sep+file
+    with open(path, 'r') as f:
         s_var=f.read()
     return s_var
 
 def main():
+    ### get log from fritzbox
     #sid = fb_get_sid(ROUTER_URL, USER, PASSWORD)
-    #log=get_log(fb_sid=sid)
-    #get_log(fb_sid=sid,file='fritz_log')
-    log=get_data_from_file('fritz_log')
+    #log=get_log_from_fb(fb_sid=sid)
+    #get_log_from_fb(fb_sid=sid, file='fritz_log.json')
+    ### get log from file
+    log=get_data_from_file('fritz_log.json')
     p_object=json.loads(log)
-    for i in get_struct(p_object):
-        print(i)
-    #for i in log:
-    #   print(f'{i}')
-    #print(f'=====\nentries in log: {len(log)}')
+    for item in get_struct(p_object):
+        print(item)
 
 if __name__ == '__main__':
     main()
