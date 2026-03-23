309bd8f85b2655437bf1d4f0627d68fcc7edfba8
Andreas Imholt <andreas@imholt.de>
2026-03-23 17:23:10 +0100

    different changes

--- image_geo_update.py
+++ image_geo_update.py
@@ -1,26 +1,29 @@
 """
-    program to update the geo coordinates in an .jpg image fpath
+    problem: my camera has no gps so no location information is stored in the images
+    this program updates the geo coordinates in a .jpg image, when a geo tracking file is given
+    - working directory is given as argument
+    - inside working directory image files and one or more gpx files are expected
+    - it's not expected that the time stamp in an image file is exactly found in an gpx file
+    - a max-time-diff is defined to set a threshold for the time difference between image timestamp and gpx timestamp
+    - the coordinates of found gpx entries are written to the exif data of the image file  
 """
+from importlib.resources import path
+from pathlib import Path
 import sys
 import os
 from argparse import ArgumentParser
 from aiPyUtilsPack.img_extensions import get_file_list, get_geo_from_gpx, get_img_timestamp, \
-                                set_geo2exif, print_exif_data
+    set_geo2exif, print_exif_data
 
 TZONE='Europe/Berlin'
-### platform dependend settings
-if sys.platform.startswith('linux'):
-    DIR='/home/anreas/projects/TestData/pictures_tracklogs'
-elif sys.platform.startswith('win'):
-    DIR='C:\\Users\\Andreas\\projects\\TestData\\pictures_tracklogs'
 
-def main():    
+def main(max_time_diff=60):
     parser=ArgumentParser(
         prog='image_geo_update',
-        description='backend-tool to update image geo data from tracking tools')
+        description='backend-tool to update image geo data with gps data from tracking tool')
     group1=parser.add_mutually_exclusive_group(required=True)
     group1.add_argument('-f', '--fname',
-                help='select only image with specified filename',
+                help='select a single image specified (filename)',
                 type=str)
     group1.add_argument('-a', '--all',
                 help='select all images in working directory',
@@ -31,23 +34,39 @@ def main():
     parser.add_argument('-s', '--save',
                 help='save changes to images',
                 action='store_true', default=False)
-    parser.add_argument('-d', '--directory',
-                help='select working directory, current is: '+DIR,
+    parser.add_argument('-d', '--directory', required=True,
+                help='set working directory',
                 type=str)
     args=parser.parse_args()
 
-    ### making image fpathlist
-    img_fpath_list=[]
+    working_dir=args.directory    
+
+    ### >>>>> check if directory exists and is readable and writable)
+    path=Path(working_dir)
+    if not path.exists():
+        sys.exit(f' Error: {path} does not exist')
+    elif not path.is_dir():
+        sys.exit(f' Error: {path} is not a directory')
+    
     if args.all:
-        img_fpath_list=get_file_list(dir=DIR,m_pattern='^.*\\.(jpg)$')
-    if args.fname:
-        img_fpath_list.append(os.path.join(DIR,args.fname))
+        img_fpath_list=get_file_list(dir=working_dir,m_pattern='^.*\\.(jpg)$')
+    elif args.fname:
+        img_fpath_list.append(os.path.join(working_dir,args.fname))
+    
+    ### making gpx and image fpath list
+    gpx_fpath_list=get_file_list(dir=working_dir,m_pattern='^.*\\.(gpx)$')
 
-    ### making gpx fpath list
-    gpx_fpath_list=get_file_list(dir=DIR,m_pattern='^.*\\.(gpx)$')
+    if len(gpx_fpath_list) == 0:
+        sys.exit(f' Error: no gpx file found in {working_dir}')
+    elif len(img_fpath_list) == 0:
+        sys.exit(f' Error: no image file found in {working_dir}')
 
-    ### making list of geo coordinates from multiple gpx fpaths sorted by time 
     geo_coord_list=[]
+
+    print(  f'working directory: {working_dir}')
+    print(  f'number of gpx files in working directory: {len(gpx_fpath_list)}')
+    print(  f'number of img files in working directory: {len(img_fpath_list)}')
+    
     for gpx_fpath in gpx_fpath_list:
         for geo_coordinate_item in get_geo_from_gpx(gpx_fpath):
             geo_coord_list.append(geo_coordinate_item)        
@@ -60,14 +79,13 @@ def main():
     count=0
     for img_fpath in img_fpath_list:
         img_dt=get_img_timestamp(img_fpath)
-        img_fname=img_fpath.lstrip(DIR)
-        max_time_diff=60
+        img_fname=img_fpath.lstrip(working_dir)
         found=False
         coord_dt_before=None
         coord_dt_after=None
         if args.exif:
             print("\n>>>>>>>>>>>>>>>>>>>>>>>>> EXIF data of image: "+img_fname)
-            print_exif_data(os.path.join(DIR,img_fpath))
+            print_exif_data(os.path.join(working_dir,img_fpath))
 
         ### for each image find the best time matches in geo coordinates
         time_diff_before=max_time_diff
