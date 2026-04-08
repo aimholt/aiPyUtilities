131209b9bee35c09764921ec2a2c7d5a98e51771
Andreas Imholt <andreas@imholt.de>
2026-04-08 22:49:02 +0200

    changes  to Kurswerte / playing with bootstrap

--- Kurswerte.py
+++ Kurswerte.py
@@ -1,31 +1,27 @@
 """ 
     Module to get current stock prices using yfinance
-    input: list withc stock of ISIN values    
+    input: stock list with stocks in json format    
 """
 import yfinance as yf
+import sys, os
+import json
 
-### YFS - yahoo finance symbol
+if sys.platform.startswith('linux'):
+    DATADIR =  "/home/andreas/projects/testData"
+else:
+    DATADIR = "C:\\Users\\Andreas\\projects\\testData"
+
+FILENAME = "mystock.json"
 
 YFS_CURR = {
         'USD':    'EUR=X'
     }
 
-MYSTOCK = [
-    {   'ISIN': 'DE0008471012', 'YFS':  'HJVD.F'},
-    {   'ISIN': 'LU0321021312', 'YFS':  'AH8S.HM'},
-    {   'ISIN': 'DE0009807016', 'YFS':  'H5AB.F'},
-    {   'ISIN': 'LU0061928585', 'YFS':  'OE7A.F'},
-    {   'ISIN': 'LU1136260384', 'YFS':  '0P00015JD5.F'},
-    {   'ISIN': 'LU0301152442', 'YFS':  'WXO3.F'},
-    {   'ISIN': 'IE00BZ02LR44', 'YFS':  'XZW0.DE'},
-    {   'ISIN': 'IE0009HF1MK9', 'YFS':  'IE0009HF1MK9.SG'},
-    {   'ISIN': 'IE00B3WJKG14', 'YFS':  'IUIT.SW'},
-    {   'ISIN': 'IE00B0M62S72', 'YFS':  'IDVY.AS'},
-    ]
+MYSTOCK = json.load(open(os.path.join(DATADIR, FILENAME), "r"))
 
 if __name__ == "__main__":
     #using yfinance to get stock prices
-    for ticker in MYSTOCK:
+    for ticker in MYSTOCK['stocks']:
         ticker_info=yf.Ticker(ticker['YFS']).info
         if ticker_info['currency'] == 'USD':
             ticker_info['regularMarketPrice'] *= yf.Ticker(YFS_CURR['USD']).info['regularMarketPrice']
